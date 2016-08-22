f688c5d0a9f9e4420f64a7e0ef7eea969ac7d937
Sindie Victor Valentin <sindievictor@gmail.com>
2016-08-23 00:32:10 +0200

    added dialogs instead of separate pages

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,5 +7,31 @@ window.onload = function() {
 		}, 100)
 		
 	}
+	
+	//11 Mottos Dialog
+	var mottosDialog = document.querySelector('#mottosDialog');
+    var showMottosDialogLink = document.querySelector('#mottosLink');
+    if (! mottosDialog.showModal) {
+      dialogPolyfill.registerDialog(mottosDialog);
+    }
+    showMottosDialogLink.addEventListener('click', function() {
+    	mottosDialog.showModal();
+    });
+    mottosDialog.querySelector('.close').addEventListener('click', function() {
+    	mottosDialog.close();
+    });
+    
+	//DOJO KUN Dialog
+	var dojoKunDialog = document.querySelector('#dojoKunDialog');
+    var showDojoKunDialogLink = document.querySelector('#dojoKunLink');
+    if (! dojoKunDialog.showModal) {
+      dialogPolyfill.registerDialog(dojoKunDialog);
+    }
+    showDojoKunDialogLink.addEventListener('click', function() {
+    	dojoKunDialog.showModal();
+    });
+    dojoKunDialog.querySelector('.close').addEventListener('click', function() {
+    	dojoKunDialog.close();
+    });
 };
 
